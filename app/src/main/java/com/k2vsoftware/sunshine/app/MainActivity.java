4aa34836686ca88bcee04d4ed77737bfbbedd62e
Kasun Kodagoda <kvkrusl@gmail.com>
2015-03-23 16:57:16 +0530

    Added HTTP connection code to the MainActivity.java to connect to the OpenWeatherMap API

--- app/src/main/java/com/k2vsoftware/sunshine/app/MainActivity.java
+++ app/src/main/java/com/k2vsoftware/sunshine/app/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -13,6 +14,12 @@ import android.os.Build;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -67,6 +74,11 @@ public class MainActivity extends ActionBarActivity {
                                  Bundle savedInstanceState) {
             View rootView = inflater.inflate(R.layout.fragment_main, container, false);
 
+            // for the OpenWeatherAPI
+            HttpURLConnection urlConnection= null;
+            BufferedReader reader = null;
+            String forecastString = null;
+
             ArrayList<String> weekForecast = new ArrayList<String>();
             weekForecast.add("Today - Sunny - 88/63");
             weekForecast.add("Tomorrow - Foggy - 70/46");
@@ -98,10 +110,67 @@ public class MainActivity extends ActionBarActivity {
                     weekForecast
             );
 
-
-
             ListView listView = (ListView) rootView.findViewById(R.id.list_view_forecast);
             listView.setAdapter(mForecastAdapter);
+
+
+
+            try {
+                URL url = new URL("http://api.openweathermap.org/data/2.5/forecast/daily?q=94043&mode=json&units=metric&cnt=7");
+
+                // Connect to the API
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("GET");
+                urlConnection.connect();
+
+                // Read the input Stream
+                InputStream inputStream = urlConnection.getInputStream();
+                StringBuffer buffer = new StringBuffer();
+
+                if(inputStream == null) {
+                    forecastString = null;
+                }
+
+                reader = new BufferedReader(new InputStreamReader(inputStream));
+
+                String line;
+
+                while ((line = reader.readLine()) != null) {
+                    buffer.append(line + "\n"); // for readability of JSON String
+                }
+
+                if(buffer.length() == 0) {
+                    forecastString = null;
+                } else {
+                    forecastString = buffer.toString();
+                }
+
+
+            } catch(IOException e) {
+                Log.e("PlaceholderFragment", "Error", e);
+                forecastString = null;
+
+            } finally {
+
+                // Close the connection
+                if(urlConnection != null) {
+                    urlConnection.disconnect();
+                }
+
+                if(reader != null) {
+                    try {
+                        reader.close();
+                    } catch (final IOException e) {
+                        Log.e("PlaceholderFragment", "Error closing stream", e);
+                    }
+                }
+
+            }
+
+
+
+
+
 
             return rootView;
         }
